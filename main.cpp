2ced5d58b913e9901b387786860ac1a58c71338f
Bothij <bothij@gmail.com>
2020-12-16 19:56:49 +0000

    working with tomb use

--- main.cpp
+++ main.cpp
@@ -2,22 +2,21 @@
 
 
 int main() {
-  int turbine_counter = 1;
-  int all_turbines = 25;
-  int turbine_power = 2000;
+  int a[25] {2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 0, 0, 0, 0, 0};
+
   int sum_power;
-  
-  for (turbine_counter = 1; turbine_counter <= all_turbines; turbine_counter++) {
-    if (turbine_counter <= 10) {
-    sum_power += turbine_power;
-    std::cout << "A(z) " << turbine_counter << ". számú szélturbina teljes fordulaton működik, " << turbine_power << " MWh-t termelve.\nA farmon termelt összenergia jelenleg " << sum_power << " MWh.\n" << std::endl;
-    } else if (turbine_counter <= 20){
-      turbine_power = 1000;
-      sum_power += turbine_power;
-    std::cout << "A(z) " << turbine_counter << ". számú szélturbina fél fordulaton működik, " << turbine_power << " MWh-t termelve.\nA farmon termelt összenergia jelenleg " << sum_power << " MWh.\n" << std::endl;
-    } else {
-      turbine_power = 0;
-      std::cout << "A(z) " << turbine_counter << ". számú szélturbina nem működik, " << turbine_power << " MWh-t termelve.\nA farmon termelt összenergia jelenleg " << sum_power << " MWh.\n" << std::endl;
+
+  for (int i = 0; i < 25; i++) {
+    
+    if (a[i] == 2000) {
+      sum_power = sum_power + a[i];
+      std::cout << "A(z) " << i + 1 << ". számú szélturbina teljes fordulaton működik, " << a[i] << " MWh-t termelve.\nA farmon termelt összenergia jelenleg " << sum_power << " MWh.\n" << std::endl;
+    } else if  (a[i] == 1000) {
+      sum_power = sum_power + a[i];
+      std::cout << "A(z) " << i + 1 << ". számú szélturbina fél fordulaton működik, " << a[i] << " MWh-t termelve.\nA farmon termelt összenergia jelenleg " << sum_power << " MWh.\n" << std::endl;
+    } else if  (a[i] == 0) {
+      sum_power = sum_power + a[i];
+      std::cout << "A(z) " << i + 1 << ". számú szélturbina nem működik, " << a[i] << " MWh-t termelve.\nA farmon termelt összenergia jelenleg " << sum_power << " MWh.\n" << std::endl;
     }
   }
   
